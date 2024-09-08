a925b1b3b9c4f680a5ad4b5cb808c319b213b51c
dylan hokstam <dylan@hokstam.com>
2024-09-08 15:16:24 +0200

    working on some simpleaudio trying to get multiple playback to work

--- python_basics/playsound.py
+++ python_basics/playsound.py
@@ -1,3 +1,24 @@
-import simpleaudio as fc
+import simpleaudio as sa
+import time
 
-fc.LeftRightCheck.run()
+a = int(0)
+
+wave_obj = sa.WaveObject.from_wave_file('python_basics/sample/[weirdloop]120.wav')
+
+
+playamount = int(input("how many times do you want the audio to play?: "))
+
+def Playsample(playamount):
+    for x in range(playamount):
+        print(f"Playing sound {x + 1}")
+        # play_obj.wait_done()
+        play_obj = wave_obj.play()
+
+        if play_obj.is_playing() == 0:
+            play_obj = wave_obj.play()
+        elif play_obj.is_playing() == 1:
+            print(x + 1)
+
+
+Playsample(playamount)
+
