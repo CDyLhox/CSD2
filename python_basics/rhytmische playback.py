8df53c2dae405caa796fbd4e7fa45677c4208aa1
dylan hokstam <dylan@hokstam.com>
2024-09-17 12:15:02 +0200

    in class updating function names

--- python_basics/rhytmische playback.py
+++ python_basics/rhytmische playback.py
@@ -3,14 +3,14 @@ import time
 
 a = int(0)
 num_playback = []
-playbacktime = []
+sequence = []
 playloop = int
 preset = []
 
 with open("python_basics/rhytminput","r") as file:
     preset = file.readlines()
     
-wave_obj = sa.WaveObject.from_wave_file('/Users/dylan/Cage/CageDotLog/CSD2/python_basics/sample/[weirdloop].wav')
+wave_obj = sa.WaveObject.from_wave_file('python_basics/sample/[weirdloop].wav')
 
 
 num_playback = (input("how many times do you want the audio to play?: "))
@@ -20,20 +20,22 @@ if num_playback == "preset1":
     bpm = 60/int(preset[1])
     playloop = int(preset[2])
     for x in range(num_playback):
-        playbacktime.append(float(preset[(x)+3]))
+        note_dur = float(preset[(x)+3])
+        sequence.append(note_dur)
 # if not then make the num_playback into an int and ask for user input
 else:
     num_playback = int(num_playback)
     bpm = 60/(int(input("and what bpm would you like? ")))
     playloop = int(input("how many times to loop? "))
     for x in range(num_playback):
-        playbacktime.append(float(input(f"time before sample {x+ 2} ")))
+        note_dur = float(input(f"time before sample {x+ 2} "))
+        sequence.append(note_dur)
 
 for y in range(playloop):
     for x in range(num_playback):
         play_obj = wave_obj.play()
         print(f"loop {y+1} sample {x+1}")
-        time.sleep(playbacktime[x] * bpm)
+        time.sleep(sequence[x] * bpm)
     
 
 
