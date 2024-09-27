5b7f3fb5f35cc1157319fa7eec0fee6caa834f64
dylan hokstam <dylan@hokstam.com>
2024-09-27 12:07:03 +0200

    adding eventSeq.py

---
+++ python_basics/eventSeq.py
@@ -0,0 +1,42 @@
+import simpleaudio as sa
+import time
+import random
+
+kick = sa.WaveObject.from_wave_file("/Users/dylan/Desktop/file wavs:mp3/noise import/samples/drum samples/cropped/kick.wav")
+snare = sa.WaveObject.from_wave_file("/Users/dylan/Desktop/file wavs:mp3/noise import/samples/drum samples/cropped/snare.wav")
+ride = sa.WaveObject.from_wave_file("/Users/dylan/Desktop/file wavs:mp3/noise import/samples/drum samples/cropped/ride.wav")
+
+instruments = [kick, snare, ride]
+
+numOfEvents = int(input("how many total events? "))
+
+kickEvents = []
+timestamp = 0
+instrumentName = ""
+instrument = sa.WaveObject
+velocity = int
+duration = int 
+
+def eventGen():
+    for i in range(numOfEvents):
+        kickEvents.append(int)
+        
+        kickEvents[i] = {
+            'timestamp': 10 + i,
+            'instrumentName': "kick",
+            'instrument': instruments[1],
+            'velocity': 82 * 10, 
+            'duration': 500 * 10  
+        }
+eventGen()
+
+def handleEvents():
+    for events in range(len(kickEvents)):
+        print(kickEvents[events]['timestamp'])    
+        print(kickEvents[events]['instrumentName'])    
+        print(kickEvents[events]['instrument'])    
+        print(kickEvents[events]['velocity'])    
+        print(kickEvents[events]['duration'])    
+        time.sleep()
+        kickEvents[events]['instrument'].play()
+handleEvents()
