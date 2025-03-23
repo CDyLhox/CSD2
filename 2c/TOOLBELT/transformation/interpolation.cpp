3f884e368d0a2cfe589aa4387f6178da6d1eaa7a
dylan hokstam <dylanhokstam@gmail.com>
2025-03-23 22:31:23 +0100

    some more repo cleaning

--- 2c/TOOLBELT/transformation/interpolation.cpp
+++ 2c/TOOLBELT/transformation/interpolation.cpp
@@ -1,41 +1,48 @@
 //
-//
-// Created by Semuel Leijten on 11/02/2025.
+// based on code by Semuel Leijten.
 //
 
 #include "interpolation.h"
 
-float Interpolation::nnMap(float value, float low, float high) {
-  if(value < 0) {
-    std::cout << "Value is lower than 0, please use values in range [0, 1]" << std::endl;
-    return low;
-  } else if(value > 1) {
-    std::cout << "Value exceeds 1, please use values in range [0, 1]" << std::endl;
-    return high;
-  }
+float Interpolation::nnMap(float value, float low, float high)
+{
+				if (value < 0) {
+								std::cout << "nnMap value is lower than 0. clamping to zero" << std::endl;
+								value = low;
+								return low * (1 - value) + high * value;
+				} else if (value > 1) {
+								std::cout << "nnMap value is higher than 1. clamping to zero" << std::endl;
+								value = high;
+								return low * (1 - value) + high * value;
+				}
 
-  if (static_cast<int>(value + 0.5f) == 0) {
-    return low;
-  }
-  return high;
+				if (static_cast<int>(value + 0.5f) == 0) {
+								return low;
+				}
+				return high;
 }
 
-float Interpolation::linMap(float value, float low, float high) {
-  if(value < 0) {
-    std::cout << "Value is lower than 0, please use values in range [0, 1]" << std::endl;
-    return low;
-  } else if (value > 1) {
-    std::cout << "Value exceeds 1, please use values in range [0, 1]" << std::endl;
-    return high;
-  }
+float Interpolation::linMap(float value, float low, float high)
+{
+				if (value < 0) {
+								std::cout << "linmap value is lower than 0. clamping to zero" << std::endl;
+								value = low;
+								return low * (1 - value) + high * value;
 
-  return low * (1 - value) + high * value;
+				} else if (value > 1) {
+								std::cout << "linmap value is higher than 1. clamping to zero" << std::endl;
+								value = high;
+								return low * (1 - value) + high * value;
+				}
+
+				return low * (1 - value) + high * value;
 }
 
-float Interpolation::mapInRange(float value, float fromLow, float fromHigh, float toLow, float toHigh) {
+float Interpolation::mapInRange(float value, float fromLow, float fromHigh, float toLow, float toHigh)
+{
 
-  float partial = (value - fromLow) / (fromHigh - fromLow);
-  float delta = toHigh - toLow;
+				float partial = (value - fromLow) / (fromHigh - fromLow);
+				float delta = toHigh - toLow;
 
-  return toLow + delta * partial;
+				return toLow + delta * partial;
 }
