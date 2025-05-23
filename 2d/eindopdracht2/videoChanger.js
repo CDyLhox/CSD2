a243814f492fc6be552e52f02619ded52837c593
dylan hokstam <dylanhokstam@gmail.com>
2025-05-23 14:10:37 +0200

    had an interesting talk and experience. starting again with the same project but a new perspective.

---
+++ 2d/eindopdracht2/videoChanger.js
@@ -0,0 +1,49 @@
+const positiveVideos = [
+	"assets/videos/1960sPlayground1.webm",
+	"assets/videos/drone.webm",
+	"assets/videos/forest.webm"
+];
+
+const negativeVideos = [
+	"assets/videos/londerSmog.webm",
+	"assets/videos/lightning.webm"
+]
+//TODO: make a single  grabnextvideo function which takes an argument for video type
+
+
+let positiveVideo = document.createElement('video');
+let negativeVideo = document.createElement('video');
+
+
+positiveVideo.id = "positiveVideoContainer";
+negativeVideo.id = "negativeVideoContainer";
+
+
+document.body.appendChild(negativeVideo);
+document.body.appendChild(positiveVideo);
+
+positiveVideo.setAttribute('src', positiveVideos[Math.floor(Math.random()*positiveVideos.length)]);
+// check if the video has ended, activate function grabNextvideo()
+document.querySelector('#positiveVideoContainer').addEventListener('ended', grabNextPositiveVideo, false);
+// log the current video to the system. 
+console.log(document.getElementById("positiveVideoContainer").getAttribute('src'))
+function grabNextPositiveVideo(e) {
+	console.log('next video');
+	
+	positiveVideoContainer.setAttribute('onloadStart', this.volume= 0);
+	positiveVideoContainer.setAttribute('src', positiveVideos[Math.floor(Math.random()*positiveVideos.length)]);
+}
+
+
+
+//same as above but for the negative Videos
+document.querySelector('#negativeVideoContainer').addEventListener('ended', grabNextNegativeVideo, false);
+// log the current video to the system. 
+console.log(document.getElementById("negativeVideoContainer").getAttribute('src'))
+function grabNextNegativeVideo(e) {
+	console.log('next  sad video');
+	
+	negativeVideoContainer.setAttribute('onloadStart', this.volume= 0);
+	negativeVideoContainer.setAttribute('src', negativeVideos[Math.floor(Math.random()*negativeVideos.length)]);
+}
+
